954467292f5bb98bc7d69d6f1b0fb3f84639f80f
Bing Hang <binghangc@gmail.com>
2026-08-22 22:52:44 +0800

    fix: Add eatOpr for '!=' equality branch

--- SimpleDBEngine/src/simpledb/parse/Parser.java
+++ SimpleDBEngine/src/simpledb/parse/Parser.java
@@ -7,77 +7,78 @@ import simpledb.record.*;
 
 /**
  * The SimpleDB parser.
+ * 
  * @author Edward Sciore
  */
 public class Parser {
    private Lexer lex;
-   
+
    public Parser(String s) {
       lex = new Lexer(s);
    }
-   
-// Methods for parsing predicates, terms, expressions, constants, and fields
-   
+
+   // Methods for parsing predicates, terms, expressions, constants, and fields
+
    public String field() {
       return lex.eatId();
    }
-   
+
    public Constant constant() {
       if (lex.matchStringConstant())
          return new Constant(lex.eatStringConstant());
       else
          return new Constant(lex.eatIntConstant());
    }
-   
+
    public Expression expression() {
       if (lex.matchId())
          return new Expression(field());
       else
          return new Expression(constant());
    }
-   
+
    public Term term() {
       Expression lhs = expression();
       /**
        * Supports =, <, <=, >, >=, !=, <>
        */
       String opr = "";
-      
+
       if (lex.matchOpr('=')) {
-    	  lex.eatOpr('=');
-    	  opr += '=';
+         lex.eatOpr('=');
+         opr += '=';
       } else if (lex.matchOpr('<')) {
-    	  lex.eatOpr('<');
-    	  opr += '<';
-    	  if (lex.matchOpr('=')) {
-    		  lex.eatOpr('=');
-    		  opr += '=';
-    	  } else if (lex.matchOpr('>')) {
-    		  lex.eatOpr('>');
-    		  opr += '>';
-    	  }
-    	  
+         lex.eatOpr('<');
+         opr += '<';
+         if (lex.matchOpr('=')) {
+            lex.eatOpr('=');
+            opr += '=';
+         } else if (lex.matchOpr('>')) {
+            lex.eatOpr('>');
+            opr += '>';
+         }
+
       } else if (lex.matchOpr('>')) {
-    	  lex.eatOpr('>');
-    	  opr += '>';
-    	  if (lex.matchOpr('=')) {
-    		  lex.eatOpr('=');
-    		  opr += '=';
-    	  }
-    	  
-    	  
+         lex.eatOpr('>');
+         opr += '>';
+         if (lex.matchOpr('=')) {
+            lex.eatOpr('=');
+            opr += '=';
+         }
+
       } else if (lex.matchOpr('!')) {
-    	  opr += '!';
-    	  if (lex.matchOpr('=')) {
-    		  lex.eatOpr('=');
-    		  opr += '=';
-    	  }
+         lex.eatOpr('!');
+         opr += '!';
+         if (lex.matchOpr('=')) {
+            lex.eatOpr('=');
+            opr += '=';
+         }
       }
-      
+
       Expression rhs = expression();
       return new Term(lhs, opr, rhs);
    }
-   
+
    public Predicate predicate() {
       Predicate pred = new Predicate(term());
       if (lex.matchKeyword("and")) {
@@ -86,9 +87,9 @@ public class Parser {
       }
       return pred;
    }
-   
-// Methods for parsing queries
-   
+
+   // Methods for parsing queries
+
    public QueryData query() {
       lex.eatKeyword("select");
       List<String> fields = selectList();
@@ -101,7 +102,7 @@ public class Parser {
       }
       return new QueryData(fields, tables, pred);
    }
-   
+
    private List<String> selectList() {
       List<String> L = new ArrayList<String>();
       L.add(field());
@@ -111,7 +112,7 @@ public class Parser {
       }
       return L;
    }
-   
+
    private Collection<String> tableList() {
       Collection<String> L = new ArrayList<String>();
       L.add(lex.eatId());
@@ -121,9 +122,9 @@ public class Parser {
       }
       return L;
    }
-   
-// Methods for parsing the various update commands
-   
+
+   // Methods for parsing the various update commands
+
    public Object updateCmd() {
       if (lex.matchKeyword("insert"))
          return insert();
@@ -134,7 +135,7 @@ public class Parser {
       else
          return create();
    }
-   
+
    private Object create() {
       lex.eatKeyword("create");
       if (lex.matchKeyword("table"))
@@ -144,9 +145,9 @@ public class Parser {
       else
          return createIndex();
    }
-   
-// Method for parsing delete commands
-   
+
+   // Method for parsing delete commands
+
    public DeleteData delete() {
       lex.eatKeyword("delete");
       lex.eatKeyword("from");
@@ -158,9 +159,9 @@ public class Parser {
       }
       return new DeleteData(tblname, pred);
    }
-   
-// Methods for parsing insert commands
-   
+
+   // Methods for parsing insert commands
+
    public InsertData insert() {
       lex.eatKeyword("insert");
       lex.eatKeyword("into");
@@ -174,7 +175,7 @@ public class Parser {
       lex.eatDelim(')');
       return new InsertData(tblname, flds, vals);
    }
-   
+
    private List<String> fieldList() {
       List<String> L = new ArrayList<String>();
       L.add(field());
@@ -184,7 +185,7 @@ public class Parser {
       }
       return L;
    }
-   
+
    private List<Constant> constList() {
       List<Constant> L = new ArrayList<Constant>();
       L.add(constant());
@@ -194,9 +195,9 @@ public class Parser {
       }
       return L;
    }
-   
-// Method for parsing modify commands
-   
+
+   // Method for parsing modify commands
+
    public ModifyData modify() {
       lex.eatKeyword("update");
       String tblname = lex.eatId();
@@ -211,9 +212,9 @@ public class Parser {
       }
       return new ModifyData(tblname, fldname, newval, pred);
    }
-   
-// Method for parsing create table commands
-   
+
+   // Method for parsing create table commands
+
    public CreateTableData createTable() {
       lex.eatKeyword("table");
       String tblname = lex.eatId();
@@ -222,7 +223,7 @@ public class Parser {
       lex.eatDelim(')');
       return new CreateTableData(tblname, sch);
    }
-   
+
    private Schema fieldDefs() {
       Schema schema = fieldDef();
       if (lex.matchDelim(',')) {
@@ -232,19 +233,18 @@ public class Parser {
       }
       return schema;
    }
-   
+
    private Schema fieldDef() {
       String fldname = field();
       return fieldType(fldname);
    }
-   
+
    private Schema fieldType(String fldname) {
       Schema schema = new Schema();
       if (lex.matchKeyword("int")) {
          lex.eatKeyword("int");
          schema.addIntField(fldname);
-      }
-      else {
+      } else {
          lex.eatKeyword("varchar");
          lex.eatDelim('(');
          int strLen = lex.eatIntConstant();
@@ -253,9 +253,9 @@ public class Parser {
       }
       return schema;
    }
-   
-// Method for parsing create view commands
-   
+
+   // Method for parsing create view commands
+
    public CreateViewData createView() {
       lex.eatKeyword("view");
       String viewname = lex.eatId();
@@ -263,10 +263,9 @@ public class Parser {
       QueryData qd = query();
       return new CreateViewData(viewname, qd);
    }
-   
-   
-//  Method for parsing create index commands
-   
+
+   // Method for parsing create index commands
+
    public CreateIndexData createIndex() {
       lex.eatKeyword("index");
       String idxname = lex.eatId();
@@ -278,4 +277,3 @@ public class Parser {
       return new CreateIndexData(idxname, tblname, fldname);
    }
 }
-
