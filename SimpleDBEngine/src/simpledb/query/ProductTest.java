f331ce2fb7cc1ef6faa30882416cc8928edd2830
Ding Yitao <yitao36g@gmail.com>
2026-09-08 13:34:43 +0800

    Rename test table name to start with temp so it gets reset

--- SimpleDBEngine/src/simpledb/query/ProductTest.java
+++ SimpleDBEngine/src/simpledb/query/ProductTest.java
@@ -3,26 +3,27 @@ package simpledb.query;
 import simpledb.server.SimpleDB;
 import simpledb.tx.Transaction;
 import simpledb.record.*;
+import simpledb.plan.*;
 
 public class ProductTest {
    public static void main(String[] args) throws Exception {
       SimpleDB db = new SimpleDB("producttest");
       Transaction tx = db.newTx();
-
+      
       Schema sch1 = new Schema();
       sch1.addIntField("A");
       sch1.addStringField("B", 9);
       Layout layout1 = new Layout(sch1);
-      TableScan ts1 = new TableScan(tx, "T1", layout1);
+      TableScan ts1 = new TableScan(tx, "tempT1", layout1);
 
       Schema sch2 = new Schema();
       sch2.addIntField("C");
       sch2.addStringField("D", 9);
       Layout layout2 = new Layout(sch2);
-      TableScan ts2 = new TableScan(tx, "T2", layout2);
+      TableScan ts2 = new TableScan(tx, "tempT2", layout2);
 
       ts1.beforeFirst();
-      int n = 200;
+      int n = 10;
       System.out.println("Inserting " + n + " records into T1.");
       for (int i=0; i<n; i++) {
          ts1.insert();
@@ -40,11 +41,11 @@ public class ProductTest {
       }
       ts2.close();
 
-      Scan s1 = new TableScan(tx, "T1", layout1);
-      Scan s2 = new TableScan(tx, "T2", layout2);
+      Scan s1 = new TableScan(tx, "tempT1", layout1);
+      Scan s2 = new TableScan(tx, "tempT2", layout2);
       Scan s3 = new ProductScan(s1, s2);
       while (s3.next())
-         System.out.println(s3.getString("B")); 
+         System.out.println(s3.getString("B") + ' ' + s3.getString("D")); 
       s3.close();
       tx.commit();
    }
