be3d89429a110d69189d4a0c73692d80acea0de7
jaye <156863812+synchrownicity@users.noreply.github.com>
2026-09-20 22:24:44 +0800

    added aggregate functions, took a stab at groupby

--- SimpleDBEngine/src/simpledb/parse/Lexer.java
+++ SimpleDBEngine/src/simpledb/parse/Lexer.java
@@ -186,6 +186,6 @@ public class Lexer {
       keywords = Arrays.asList("select", "from", "where", "and", "order", "by", "asc", "desc",
             "insert", "into", "values", "delete", "update", "set",
             "create", "table", "int", "varchar", "view", "as", "index", "on",
-            "using", "hash", "btree");
+            "using", "hash", "btree", "sum", "count", "avg", "min", "max", "group");
    }
 }
